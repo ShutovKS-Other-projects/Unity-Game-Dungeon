23c18163e1a3de20f70df0bc9cc5b7c35682176c
ShutovKS <i@shutovks.ru>
2023-03-13 10:18:57 +0300

    Added switch UI skills & inventory

---
+++ Assets/Internal assets/Scripts/UI/PlayerInfo/Main.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace UI.PlayerInfo
+{
+    public class Main : MonoBehaviour
+    {
+        [SerializeField] private GameObject uiInventory;
+        [SerializeField] private GameObject uiSkills;
+
+        private void OnEnable()
+        {
+            OnInventory();
+        }
+        
+        private void OnDisable()
+        {
+            OnInventory();
+        }
+
+        public void OnInventory()
+        {
+            uiInventory.SetActive(true);
+            uiSkills.SetActive(false);
+        }
+        
+        public void OnSkills()
+        {
+            uiInventory.SetActive(false);
+            uiSkills.SetActive(true);
+        }
+    }
+}
