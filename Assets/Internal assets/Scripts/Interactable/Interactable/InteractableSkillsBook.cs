86ea131707db2415a62bd72f53073047b71d7b16
ShutovKS <i@shutovks.ru>
2023-03-20 12:10:02 +0300

    Added InteractableSkillsBook

---
+++ Assets/Internal assets/Scripts/Interactable/Interactable/InteractableSkillsBook.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Interactable.Interactable
+{
+    public class InteractableSkillsBook : InteractableBase
+    {
+        public override void OnInteract()
+        {
+            Debug.Log("InteractableSkillsBook");
+        }
+    }
+}
